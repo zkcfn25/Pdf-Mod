28b0fcfea095dde04cc250bcfc145d3ab77d8dd2
zkcfn25 <36310269+zkcfn25@users.noreply.github.com>
2021-07-01 10:14:53 -0400

    Add files via upload

---
+++ PdfModification/PdfModification/Images.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfModification
+{
+    public class Images
+    {
+        public string ImageID { get; set; }
+        public string AttachmentName { get; set; }
+        public string Keys { get; set; }
+        public string DocumentType { get; set; }
+        public string DbId { get; set; }
+        public string DbName { get; set; }
+        public byte[] Image { get; set; }
+        public string FileType { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
